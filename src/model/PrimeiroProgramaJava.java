7dc6857cd36b164782b1c42f88d2378e1989ec40
Vinicius Mombach <mombachvinicius@gmail.com>
2021-11-07 22:16:42 -0300

    First commit ;)

---
+++ src/model/PrimeiroProgramaJava.java
@@ -0,0 +1,17 @@
+package model;
+
+import model.Gato;
+
+public class PrimeiroProgramaJava {
+    public static void main(String[] args) {
+    Gato gato = new Gato();
+
+        System.out.println(gato);
+
+
+        //        int a = 3;
+//        int b= 5;
+//
+//        System.out.println("Hello World " + (a+b));
+    }
+}
